50d38d663c75274c5c477c2aaf6acc3cbfc7eeaa
PettLe <petewaterz@gmail.com>
2021-11-05 11:05:24 +0200

    testing ways to filter information and how to render specific things

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import "regenerator-runtime/runtime";
 
-/*function getCity() {
-    prompt
-}*/
-
 async function getWeather() {
   const city = prompt("City?");
   const response = await fetch(
@@ -12,7 +8,16 @@ async function getWeather() {
       "&APPID=82d857531cd7e980cf7eb2bfca144bfb",
     { mode: "cors" }
   );
-  const weather = await response.json();
-  console.log(weather);
+  const currentWeather = await response.json();
+  console.log(currentWeather);
+  //const clouds = await currentWeather.weather[0];
+  //console.log(clouds);
+  const weatherIcon = await currentWeather.weather[0].icon;
+  console.log(weatherIcon);
+
+  const container = document.getElementById("container");
+  const img = document.createElement("img");
+  img.src = "http://openweathermap.org/img/w/" + weatherIcon + ".png";
+  container.appendChild(img);
 }
 getWeather();
