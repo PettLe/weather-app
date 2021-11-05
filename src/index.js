c589a3192b9c77e79040ea33df48a19659f54e4f
PettLe <petewaterz@gmail.com>
2021-11-05 11:39:42 +0200

    fixed ESlint and made console show temperature in celsius

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
+/* eslint-disable no-alert */
 import "regenerator-runtime/runtime";
 
 async function getWeather() {
   const city = prompt("City?");
   const response = await fetch(
-    "http://api.openweathermap.org/data/2.5/weather?q=" +
-      city +
-      "&APPID=82d857531cd7e980cf7eb2bfca144bfb",
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=82d857531cd7e980cf7eb2bfca144bfb`,
+    // eslint-disable-next-line comma-dangle
     { mode: "cors" }
   );
   const currentWeather = await response.json();
   console.log(currentWeather);
-  //const clouds = await currentWeather.weather[0];
-  //console.log(clouds);
+  //  const clouds = await currentWeather.weather[0];
+  //  console.log(clouds);
   const weatherIcon = await currentWeather.weather[0].icon;
-  console.log(weatherIcon);
+
+  const temp = await currentWeather.main.temp;
+  console.log(`${(temp - 273).toFixed(1)} celsius`);
 
   const container = document.getElementById("container");
   const img = document.createElement("img");
-  img.src = "http://openweathermap.org/img/w/" + weatherIcon + ".png";
+  img.src = `http://openweathermap.org/img/w/${weatherIcon}.png`;
   container.appendChild(img);
 }
 getWeather();
