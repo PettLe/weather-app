8b847a7a0d31f9a6539cfe68f5a409fcb2dff702
PettLe <petewaterz@gmail.com>
2021-11-05 10:39:55 +0200

    created function to take a city and console log that location's weather

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import "regenerator-runtime/runtime";
 
-console.log("toimiiko?");
+/*function getCity() {
+    prompt
+}*/
 
 async function getWeather() {
+  const city = prompt("City?");
   const response = await fetch(
-    "http://api.openweathermap.org/data/2.5/weather?q=helsinki&APPID=82d857531cd7e980cf7eb2bfca144bfb",
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&APPID=82d857531cd7e980cf7eb2bfca144bfb",
     { mode: "cors" }
   );
   const weather = await response.json();
-  //console.log(response.json());
   console.log(weather);
 }
 getWeather();
