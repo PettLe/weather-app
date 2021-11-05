712df617801da6948fa054455ab4e2c73a55e339
PettLe <petewaterz@gmail.com>
2021-11-05 12:35:56 +0200

    found way to check for five day forecast

--- src/index.js
+++ src/index.js
@@ -22,4 +22,28 @@ async function getWeather() {
   img.src = `http://openweathermap.org/img/w/${weatherIcon}.png`;
   container.appendChild(img);
 }
+
+async function getWeatherLong() {
+  const response = await fetch(
+    "http://api.openweathermap.org/data/2.5/forecast?q=helsinki&&units=metric&cnt=5&APPID=82d857531cd7e980cf7eb2bfca144bfb",
+    // eslint-disable-next-line comma-dangle
+    { mode: "cors" }
+  );
+
+  const currentWeather = await response.json();
+  console.log(currentWeather);
+  //  const kokeilu = await currentWeather.weather;
+  //  console.log(kokeilu);
+  //  const weatherIcon = await currentWeather.weather[0].icon;
+
+  //  const temp = await currentWeather.main.temp;
+  //  console.log(`${temp} celsius`);
+
+  //  const container = document.getElementById("container");
+  //  const img = document.createElement("img");
+  //  img.src = `http://openweathermap.org/img/w/${weatherIcon}.png`;
+  //  container.appendChild(img);
+}
+
 getWeather();
+getWeatherLong();
