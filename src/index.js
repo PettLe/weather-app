72d4f1b8e7cc58ac1f6b66c2a6760584e338fcf4
PettLe <petewaterz@gmail.com>
2021-11-05 10:30:44 +0200

    had a fight with Babel. Fixed it. Also created function that console logs weather

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import "regenerator-runtime/runtime";
+
 console.log("toimiiko?");
+
+async function getWeather() {
+  const response = await fetch(
+    "http://api.openweathermap.org/data/2.5/weather?q=helsinki&APPID=82d857531cd7e980cf7eb2bfca144bfb",
+    { mode: "cors" }
+  );
+  const weather = await response.json();
+  //console.log(response.json());
+  console.log(weather);
+}
+getWeather();
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: ["regenerator-runtime/runtime.js", "./src/index.js"],
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
